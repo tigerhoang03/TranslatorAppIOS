9b1738ef5bf1d256dd73c9987e11c47263fbc298
Aman Sahu <144287512+as9219@users.noreply.github.com>
2024-06-04 19:00:17 -0400

    Added Settings Page
    added very raw settings page

---
+++ translatorDraft/settingScreen.swift
@@ -0,0 +1,14 @@
+//
+//  settingScreen.swift
+//  translatorDraft
+//
+//  Created by Aman Sahu on 6/4/24.
+//
+
+import SwiftUI
+
+struct settingScreen: View {
+    var body: some View {
+        Text("This is the settings page")
+    }
+}
--- translatorDraft/test.swift
+++ translatorDraft/test.swift
@@ -21,150 +21,155 @@ struct test: View {
 //    var targetLanguageCode: String { languageDirection ? language[selectedTargetLanguage]! : language[selectedSourceLanguage]! }
     
     var body: some View {
-        ZStack {
-            Color.background
-                .ignoresSafeArea()
-            VStack {
-                HStack{
-                    Spacer()
-                    Image(systemName: "bell.badge")
-                        .padding()
-                        .frame(width: 45, height: 45)
-                        .foregroundColor(.txtColors)
-                    
-                    Text("Communicator")
-                        .frame(width: 220, height: 50)
-                        .font(.title.bold())
-                        .foregroundColor(.txtColors)
-                    
-                    
-                    Button(action: viewModel.clearText) {
-                        Text("Clear")
-                            .padding()
-                            .background(Color.btnColors)
+        NavigationView {
+            ZStack {
+                Color.background
+                    .ignoresSafeArea()
+                VStack {
+                    HStack{
+                        Spacer()
+                        NavigationLink(destination: settingScreen()) {
+                            Image(systemName: "gear")
+                                .padding()
+                                .frame(width: 45, height: 45)
+                                .foregroundColor(.txtColors)
+                        }
+                        
+                        Text("Communicator")
+                            .frame(width: 220, height: 50)
+                            .font(.title.bold())
                             .foregroundColor(.txtColors)
-                            .cornerRadius(15)
-                            .scaleEffect(viewModel.clearPressed ? 1.2 : 1.0)
-                            .gesture(
-                                DragGesture(minimumDistance: 0)
-                                    .onChanged { _ in
-                                        withAnimation {
-                                            viewModel.clearPressed = true
+                        
+                        
+                        Button(action: viewModel.clearText) {
+                            Text("Clear")
+                                .padding()
+                                .background(Color.btnColors)
+                                .foregroundColor(.txtColors)
+                                .cornerRadius(15)
+                                .scaleEffect(viewModel.clearPressed ? 1.2 : 1.0)
+                                .gesture(
+                                    DragGesture(minimumDistance: 0)
+                                        .onChanged { _ in
+                                            withAnimation {
+                                                viewModel.clearPressed = true
+                                            }
                                         }
-                                    }
-                                    .onEnded { _ in
-                                        withAnimation {
-                                            viewModel.clearPressed = false
+                                        .onEnded { _ in
+                                            withAnimation {
+                                                viewModel.clearPressed = false
+                                            }
+                                            viewModel.clearText()
                                         }
-                                        viewModel.clearText()
-                                    }
+                                )
+                        }
+                        Spacer()
+                    }
+                    
+                    Divider()
+                    
+                    HStack {
+                        
+                        Picker("FIRST LANGUAGE", selection: $viewModel.selectedSourceLanguage) {
+                            ForEach(languages, id: \.self) { language in
+                                Text(language)
+                            }
+                        }.padding(EdgeInsets())
+                            .background(
+                                RoundedRectangle(cornerRadius: 25.0)
+                                    .fill(.textBoxColors)
                             )
-                    }
-                    Spacer()
+                        
+                        Button {
+                            withAnimation {
+                                viewModel.languageDirection.toggle()
+                            }
+                        } label: {
+                            Image(systemName: viewModel.languageDirection ? "arrow.right" : "arrow.left")
+                        }
+                        .foregroundColor(.highlighting)
+                        
+                        Picker("SECOND LANGUAGE", selection: $viewModel.selectedTargetLanguage) {
+                            ForEach(languages, id: \.self) { language in
+                                Text(language)
+                            }
+                        }.padding(EdgeInsets())
+                            .background(
+                                RoundedRectangle(cornerRadius: 25.0)
+                                    .fill(.textBoxColors)
+                            )
+                    }.padding()
+                    
+                    //                Text("first: \(viewModel.sourceLanguageCode),\nsecond: \(viewModel.targetLanguageCode),\ndirection:\(viewModel.languageDirection)")
+                    //                    .font(.caption2)
+                    
+                    // First TextField (Language 1)
+                    ZStack(alignment: .top) {
+                        RoundedRectangle(cornerRadius: 25.0)
+                            .fill(.textBoxColors)
+                            .frame(width: 375, height: 200)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 25.0)
+                                    .stroke(Color.highlighting, lineWidth: 3)
+                                    .opacity(viewModel.languageDirection ? 0 : 1)
+                            )
+                            .animation(.easeInOut, value: viewModel.languageDirection)
+                        
+                        TextField("", text: $viewModel.inputText, prompt: Text("Language 1 \(viewModel.selectedSourceLanguage)").foregroundColor(.gray), axis: .vertical)
+                            .lineLimit(7)
+                            .padding()
+                            .focused($isFocused1) // Bind the focus state
+                    }.overlay(
+                        VStack() {
+                            if isFocused1 {
+                                Button("Done") {
+                                    hideKeyboard()
+                                }
+                                .padding(.trailing, 20)
+                                .padding(.bottom, 15)
+                                .transition(.opacity) // Optional: Add a fade transition
+                            }
+                        }, alignment: .bottomTrailing
+                    )
+                    .padding()
+                    
+                    testAudioTranslationHandler(viewModel: viewModel)
+                    
+                    // Second TextField (Language 2)
+                    ZStack(alignment: .top) {
+                        RoundedRectangle(cornerRadius: 25.0)
+                            .fill(.textBoxColors)
+                            .frame(width: 375, height: 200)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 25.0)
+                                    .stroke(Color.highlighting, lineWidth: 3)
+                                    .opacity(viewModel.languageDirection ? 1 : 0)
+                            )
+                            .animation(.easeInOut, value: viewModel.languageDirection)
+                        
+                        TextField("", text: $viewModel.outputText, prompt: Text("Language 2 \(viewModel.selectedTargetLanguage)").foregroundColor(.gray), axis: .vertical)
+                            .lineLimit(7)
+                            .padding()
+                            .focused($isFocused2) // Bind the focus state
+                    }.overlay(
+                        VStack() {
+                            if isFocused2 {
+                                Button("Done") {
+                                    hideKeyboard()
+                                }
+                                .padding(.trailing, 20)
+                                .padding(.top, 15)
+                                .transition(.opacity) // Optional: Add a fade transition
+                            }
+                        }, alignment: .topTrailing
+                    )
+                    .padding()
+                    
+                    
                 }
-                
-                Divider()
-                
-                HStack {
-                    
-                    Picker("FIRST LANGUAGE", selection: $viewModel.selectedSourceLanguage) {
-                        ForEach(languages, id: \.self) { language in
-                            Text(language)
-                        }
-                    }.padding(EdgeInsets())
-                        .background(
-                            RoundedRectangle(cornerRadius: 25.0)
-                                .fill(.textBoxColors)
-                        )
-                    
-                    Button {
-                        withAnimation {
-                            viewModel.languageDirection.toggle()
-                        }
-                    } label: {
-                        Image(systemName: viewModel.languageDirection ? "arrow.right" : "arrow.left")
-                    }
-                    .foregroundColor(.highlighting)
-                    
-                    Picker("SECOND LANGUAGE", selection: $viewModel.selectedTargetLanguage) {
-                        ForEach(languages, id: \.self) { language in
-                            Text(language)
-                        }
-                    }.padding(EdgeInsets())
-                        .background(
-                            RoundedRectangle(cornerRadius: 25.0)
-                                .fill(.textBoxColors)
-                        )
-                }.padding()
-                
-//                Text("first: \(viewModel.sourceLanguageCode),\nsecond: \(viewModel.targetLanguageCode),\ndirection:\(viewModel.languageDirection)")
-//                    .font(.caption2)
-                
-                // First TextField (Language 1)
-                ZStack(alignment: .top) {
-                    RoundedRectangle(cornerRadius: 25.0)
-                        .fill(.textBoxColors)
-                        .frame(width: 375, height: 200)
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 25.0)
-                                .stroke(Color.highlighting, lineWidth: 3)
-                                .opacity(viewModel.languageDirection ? 0 : 1)
-                        )
-                        .animation(.easeInOut, value: viewModel.languageDirection)
-                    
-                    TextField("", text: $viewModel.inputText, prompt: Text("Language 1 \(viewModel.selectedSourceLanguage)").foregroundColor(.gray), axis: .vertical)
-                        .lineLimit(7)
-                        .padding()
-                        .focused($isFocused1) // Bind the focus state
-                }.overlay(
-                    VStack() {
-                        if isFocused1 {
-                            Button("Done") {
-                                hideKeyboard()
-                            }
-                            .padding(.trailing, 20)
-                            .padding(.bottom, 15)
-                            .transition(.opacity) // Optional: Add a fade transition
-                                                }
-                }, alignment: .bottomTrailing
-                )
-                .padding()
-                
-                testAudioTranslationHandler(viewModel: viewModel)
-                
-                // Second TextField (Language 2)
-                ZStack(alignment: .top) {
-                    RoundedRectangle(cornerRadius: 25.0)
-                        .fill(.textBoxColors)
-                        .frame(width: 375, height: 200)
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 25.0)
-                                .stroke(Color.highlighting, lineWidth: 3)
-                                .opacity(viewModel.languageDirection ? 1 : 0)
-                        )
-                        .animation(.easeInOut, value: viewModel.languageDirection)
-                    
-                    TextField("", text: $viewModel.outputText, prompt: Text("Language 2 \(viewModel.selectedTargetLanguage)").foregroundColor(.gray), axis: .vertical)
-                        .lineLimit(7)
-                        .padding()
-                        .focused($isFocused2) // Bind the focus state
-                }.overlay(
-                    VStack() {
-                        if isFocused2 {
-                            Button("Done") {
-                                hideKeyboard()
-                            }
-                            .padding(.trailing, 20)
-                            .padding(.top, 15)
-                            .transition(.opacity) // Optional: Add a fade transition
-                                                }
-                }, alignment: .topTrailing
-                )
-                .padding()
-                
-                
             }
         }
+        .navigationTitle("Home")
     }
     
     func hideKeyboard() {
