ee5ff82bc6b513e4a2eb6f9e84d27448e4a3d982
Aman Sahu <144287512+as9219@users.noreply.github.com>
2024-06-04 19:51:53 -0400

    added card views in settings screen

--- translatorDraft/settingScreen.swift
+++ translatorDraft/settingScreen.swift
@@ -8,11 +8,29 @@
 import SwiftUI
 
 struct SettingScreen: View {
+    
+    @State private var selectedPlan: String? = nil
+    let plans = ["Free Plan", "Premium Plan"]
+    
     var body: some View {
         NavigationView {
             ZStack {
                 Color.background.ignoresSafeArea()
                 List {
+                    Section(header: Text("Plan")) {
+                        ScrollView(.vertical) {
+                            LazyHGrid(rows: [GridItem(.flexible())], spacing: 20) {
+                                ForEach(plans, id: \.self) { plan in
+                                    CardView(plan: plan, isSelected: plan == selectedPlan)
+                                        .onTapGesture {
+                                            selectedPlan = plan
+                                        }
+                                }
+                            }
+                            .padding()
+                        }
+                    }
+                    
                     Section(header: Text("General")) {
                         NavigationLink(destination: AboutScreen()) {
                             Text("How To Use")
@@ -36,6 +54,38 @@ struct SettingScreen: View {
             }
             .navigationTitle("Settings")
         }
+    }
+}
+
+
+struct CardView: View {
+    let plan: String
+    let isSelected: Bool
+    
+    var body: some View {
+        VStack {
+            Text(plan)
+                .font(.title3)
+                .fontWeight(.bold)
+                .foregroundColor(.primary)
+                .padding()
+            
+            if isSelected {
+                Image(systemName: "checkmark.circle.fill")
+                    .foregroundColor(.highlighting)
+                    .font(.title)
+                    .padding(.bottom, 5)
+            } else {
+                Image(systemName: "circle")
+                    .foregroundColor(.secondary)
+                    .font(.title)
+                    .padding(.bottom, 5)
+            }
+        }
+        .background(Color.white)
+        .cornerRadius(10)
+        .shadow(color: Color.gray.opacity(0.4), radius: 4, x: 0, y: 2)
+        .padding([.horizontal, .top])
     }
 }
 
